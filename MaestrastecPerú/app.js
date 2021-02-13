e72d21b1491bfc96627ecdca88cb6758a48697ab
ashel1806 <71417101+ashel1806@users.noreply.github.com>
2021-02-13 06:35:33 -0500

    Delete MaestrastecPerú directory

--- MaestrastecPerú/app.js
+++
@@ -1,8 +0,0 @@
-function cambiarClase(){
-    let siteNav = document.getElementById('site-nav');
-        siteNav.classList.toggle('site-nav-open');
-
-    let menuOpen = document.getElementById('menu-toogle');
-        menuOpen.classList.toggle('menu-open');
-}
-
